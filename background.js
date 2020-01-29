af874dc1fe1c922231aa8ce40adb92a66f05fa3e
kaixa <690750353@qq.com>
2020-01-30 00:34:29 +0800

    添加注释便于编辑器认识数据类型

--- background.js
+++ background.js
@@ -4,8 +4,8 @@
  *   Uncaught SyntaxError: Cannot use import statement outside a module
  * 加上函数的实现极其简单，我懒得配置webpack，所以就在两个文件中定义了相同的
  * 函数。
- * @param {*} type 消息类型
- * @param {*} content 消息内容
+ * @param {string} type 消息类型
+ * @param {any} content 消息内容
  */
 const msg = (type, content) => {
     return {
@@ -25,14 +25,21 @@ class VpnConnector {
 
     constructor() {
     }
-
+    /**
+     * 设置登录信息
+     * @param {string} loginId 
+     * @param {string} password 
+     */
     setLoginInfo(loginId, password) {
         this.id = loginId
         this.pwd = password
         this.postData = `method=&uname=${this.id}&pwd1=${this.pwd}&pwd2=
             &pwd=${this.pwd}&submitbutton=%E7%99%BB%E5%BD%95`
     }
-
+    /**
+     * 生成以 https://vpn.bjut.edu.cn/prx/000/ 为前缀的重定向链接
+     * @param {string} url 
+     */
     static makeRedirectUrl(url) {
         if (detail.url.indexOf('http://') != -1) {
             alert('链接将重定向至:https://vpn.bjut.edu.cn/prx/000/http/' +
@@ -83,18 +90,25 @@ class VpnConnector {
         request.send(postData)
     }
 
+    /**
+     * 判断登录id是否合法。应为8位数字
+     * @param {string} loginId 
+     */
     static isValidId(loginId) {
         return /^\d{8}$/.test(loginId)
     }
 
 }
 
-
 let connector = null
 document.addEventListener('DOMContentLoaded', () => {
     connector = new VpnConnector()
 })
 
+/**
+ * 处理登录请求
+ * @param {{id:string,pwd:string}} content 
+ */
 const handleLogin = (content) => {
     let id = content.id
     let password = content.pwd
@@ -107,6 +121,10 @@ const handleLogin = (content) => {
     }
 }
 
+/**
+ * 处理查询请求
+ * @param {{what:string}} content 查询内容
+ */
 const handleQuery = (content) => {
     if (content.what === 'connection status') {
         return VpnConnector.ConnectionStatus
@@ -119,6 +137,12 @@ const handler = {
     query: handleQuery
 }
 
+/**
+ * 接收vpn_login消息的监听器。
+ * @param {{type:string,content:any}} message 
+ * @param {*} sender 
+ * @param {function ({msg:any}) } sendResponse 使用这个函数回应消息
+ */
 const onMessageListener = (message, sender, sendResponse) => {
     const type = message.type
     const content = message.content
--- vpn_login.js
+++ vpn_login.js
@@ -1,3 +1,8 @@
+/**
+ * 通过id获取HTML元素，有点像JQuery那个$。但是想到没准以后要用JQuery，所以
+ * 换了个名字。直接调这个函数能少写一笔是一笔。
+ * @param {string} id 
+ */
 const getElement = (id) => {
     return document.getElementById(id)
 }
@@ -8,8 +13,8 @@ const getElement = (id) => {
  *   Uncaught SyntaxError: Cannot use import statement outside a module
  * 加上函数的实现极其简单，我懒得配置webpack，所以就在两个文件中定义了相同的
  * 函数。
- * @param {*} type 消息类型
- * @param {*} content 消息内容
+ * @param {string} type 消息类型
+ * @param {any} content 消息内容
  */
 const msg = (type, content) => {
     return {
@@ -20,18 +25,36 @@ const msg = (type, content) => {
 
 let connectButton = getElement('connect')
 
+/**
+ * 将VpnConnector的ConnectionStatus转为数组下标。与StatusBoxes配合使用，则可以
+ * 根据不同的连接状态显示不同的图片。
+ */
 const StatusToCode = {
     not_connected: 0,
     connecting: 1,
     connected: 2
 }
 
+/**
+ * HTML中用于显示状态的元素集合。第一个是null因为未连接状态什么都不显示。第二个是正在连接的那个转圈图片。
+ * 第三个是已连接的绿色对勾。与StatusToCode使用可以实现根据 VpnConnector.ConnectionStatus 获取对应的
+ * HTML元素。
+ * @example
+ *  从background获取了当前连接状态 s
+ *      let s = 'connecting'
+ *      let box = StatusBoxes[StatusToCode[s]]
+ *  box就是vpn_login.html中id为'statusConnecting'的元素，也就是那个转圈的。
+ */
 const StatusBoxes = [
     null,
     getElement('statusConnecting'),
     getElement('statusConnected')
 ]
 
+/**
+ * 改变vpn_login.html中显示的连接状态
+ * @param {string} status 想要页面显示的连接状态
+ */
 const changeDisplayedStatus = (status) => {
     StatusBoxes.forEach((element) => {
         if (element !== null) {
@@ -44,6 +67,9 @@ const changeDisplayedStatus = (status) => {
     }
 }
 
+/**
+ * 向background获取Vpn的连接状态，并根据获得的状态改变网页中显示的状态
+ */
 const queryConnectionStatus = () => {
     chrome.runtime.sendMessage(msg('query', { what: 'connection status' }), (response) => {
         let status = response.msg
@@ -55,6 +81,9 @@ const queryConnectionStatus = () => {
     })
 }
 
+/**
+ * 提交按钮点击事件。向background发送登录用的数据。
+ */
 connectButton.addEventListener('click', () => {
     changeDisplayedStatus('connecting')
     let loginInfo = {
@@ -69,15 +98,25 @@ connectButton.addEventListener('click', () => {
     })
 })
 
+/**
+ * 接收background消息的监听器。
+ * @param {{type:string,content:any}} message 
+ * @param {*} sender 
+ * @param {function ({msg:any}) } sendResponse 使用这个函数回应消息
+ */
 const onMessageListener = (message, sender, sendResponse) => {
     if (message.type === 'change_status') {
         changeDisplayedStatus(message.content)
         connectButton.disabled = true
     }
+    sendResponse()
 }
 
 chrome.runtime.onMessage.addListener(onMessageListener)
 
+/**
+ * 向DOM添加监听器，DOM加载完成时向background查询Vpn连接状态
+ */
 document.addEventListener('DOMContentLoaded', () => {
     queryConnectionStatus()
 })
